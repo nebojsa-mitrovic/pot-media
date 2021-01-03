58744a96e8fdc43aada1cf3da4858ce3897ca2aa
nebojsa-mitrovic <32269043+Nebojsa-nm-d@users.noreply.github.com>
2021-01-03 16:05:24 +0100

    Adding Text Animation on Homepage

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,7 +2,7 @@
  * Main Javascript File
  */
 
-// // Displaying Homepage after 5 seconds
+// Displaying Homepage after 5 seconds
 setTimeout(function loader() {
 	document.querySelector("body").classList.add("loaded");
 }, 5000);
